cd4c62eb7a158ceca3c254a7a91eeaa69a5ce7bb
Charles Bushong <cbushong@fearless.tech>
2020-02-20 14:28:27 -0500

    IA-1299: Adding lambda arn to the outputs

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,16 @@ output "sns_green" {
 output "sns_email_admins" {
   value = var.enabled ? aws_sns_topic.email_admins[0] : null
 }
+
+output "lambda_yellow_arn" {
+  value = var.enabled ? module.notify_slack_yellow.notify_slack_lambda_function_arn : null
+}
+output "lambda_red_arn" {
+  value = var.enabled ? module.notify_slack_red.notify_slack_lambda_function_arn : null
+}
+output "lambda_security_arn" {
+  value = var.enabled ? module.notify_slack_security.notify_slack_lambda_function_arn : null
+}
+output "lambda_green_arn" {
+  value = var.enabled ? module.notify_slack_green.notify_slack_lambda_function_arn : null
+}
